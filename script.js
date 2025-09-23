193b006075ed4730f92769e1d0904c0319594c5c
IslamAsankojoev <isla.asankojoev@mail.ru>
2025-09-24 00:17:54 +0600

    added success modal

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
-const SELECTOR = '.amoforms__form, #amoforms_widget, iframe[src*="forms.amocrm"]'
-function applyStyles() {
-  const input = document.querySelector('.amoforms__field__control')
-  if (input) {
-    inputStyle(input)
-  }
+function showSuccessPopup() {
+  const successPopup = document.querySelector(".success-popup")
+  successPopup.classList.add("active")
 }
 
-function inputStyle(btn) {
-  btn.style.borderRadius = '600px'
+function closeSuccessPopup() {
+  const successPopup = document.querySelector(".success-popup")
+  successPopup.classList.remove("active")
 }
 
-;(function waitAndStyle(maxTries = 150, intervalMs = 200) {
-  let tries = 0
-  const timer = setInterval(() => {
-    const form = document.querySelector(SELECTOR)
-    if (form) {
-      clearInterval(timer)
-      // если это iframe — ждём загрузку (на всякий случай)
-      if (form.tagName === 'IFRAME') {
-        if (form.complete) applyStyles(form)
-        else form.addEventListener('load', () => applyStyles(form), { once: true })
-      } else {
-        applyStyles(form)
-      }
-    } else if (++tries >= maxTries) {
-      clearInterval(timer)
-      console.warn('AMO-форма не найдена по селектору:', SELECTOR)
+window.addEventListener("message", (event) => {
+  if (event.origin.includes("amocrm.ru")) {
+    const data = JSON.parse(event.data)
+    if (data.func === "amoformsSuccessSubmit") {
+      showSuccessPopup()
     }
-  }, intervalMs)
-})()
+  }
+});
